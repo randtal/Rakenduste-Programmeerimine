b9f0713efb87465884259dfdc8076645a8ffc4ad
Rando Talviste <randotalviste@live.com>
2019-09-27 10:07:48 +0300

    Lesson 3 half way

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      {
+        from: "public"
+      }
+  ])
+],
+module: {
+   rules: [
+     {
+       test: /\.(js|jsx)$/,
+       exclude: /node_modules/,
+       use: 'babel-loader'
+     }
+  ]
+ },
+devServer: {
+  contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+}
+};
