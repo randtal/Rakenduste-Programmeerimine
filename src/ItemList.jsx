b944396c8de57d0b86e9948f6ced896a93b81acd
Rando Talviste <randotalviste@live.com>
2019-09-27 11:48:35 +0300

    Third lesson, currently broken

--- src/ItemList.jsx
+++ src/ItemList.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 
-const Item = [
-  {
-    name: "item1",
-    cost: 200,
-  },
-  {
-    name: "item2",
-    cost: 300,
-  }
-];
-
-const ItemList = () => {
+const ItemList = (props) => {
   return (
-    <div>Item list</div>
+    <div className={"content"}>
+      {
+        props.items.map( item => {
+          return <Item
+              imgSrc={item.imgSrc}
+              price={item.price}
+              title={item.title}
+            />
+          })
+      }
+    </div>
   )
 };
+
+const Item = (props) => {
+
+    return (
+        <div className = {"item"}>
+          <img src={props.imgSrc}/>
+          <div className="item__title">{props.title}</div>
+          <div className="item__price">{props.price}</div>
+        </div>
+      )
+  };
 
 export default ItemList;
