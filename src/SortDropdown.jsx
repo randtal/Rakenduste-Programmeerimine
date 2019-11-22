06ee2d359571011610c7d91502a807b8f39ff17f
Rando Talviste <randot@tlu.ee>
2019-11-22 02:44:51 +0200

    test

---
+++ src/SortDropdown.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dropdown = () => (
+    <div>
+        <select>
+            <option>Price high to low</option>
+            <option>Price low to high</option>
+        </select>
+    </div>
+);
+
+export default Dropdown;
